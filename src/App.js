ccba32d9864d0ba91d65dc0af369a9df8ecae1cb
Tarun Kandikunta <vkandiku@uncc.edu>
2024-09-28 01:06:11 -0400

    Implemented Date Counter using step and count

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date();
+  const newDate = new Date(date.setDate(date.getDate() + count)).toDateString();
+
+  function stepDecrement() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function stepIncrement() {
+    setStep((s) => s + 1);
+  }
+
+  function countDecrement() {
+    setCount((s) => s - step);
+  }
+  function countIncrement() {
+    setCount((s) => s + step);
+  }
+  function reset() {
+    setStep(1);
+    setCount(0);
+  }
+
+  return (
+    <div className="center">
+      <div>
+        <div className="step">
+          <button onClick={stepDecrement}>-</button>
+          <p className="stepCount">Steps: {step}</p>
+          <button onClick={stepIncrement}>+</button>
+        </div>
+        <div className="count">
+          <button onClick={countDecrement}>-</button>
+          <p className="stepCount">Count: {count}</p>
+          <button onClick={countIncrement}>+</button>
+        </div>
+      </div>
+      <div className="dateCounter">
+        <span>
+          {count === 0
+            ? `Today is ${newDate}`
+            : count > 0
+            ? `${count} days from today is ${newDate}`
+            : `${Math.abs(count)} days ago was ${newDate}`}
+        </span>
+      </div>
+      <div className="reset">
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
+  );
+}
